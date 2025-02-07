6d8224a4dc766063fe0d2dc5975bd78918789190
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2025-02-07 18:50:45 -0500

    Scenario 1: Show Upcoming Events completed

---
+++ src/components/Event.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Event = () => {
+    return (
+        <li></li>
+    );
+}
+
+export default Event;
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,12 +1,16 @@
-// src/components/EventList.js
+// src/components/EventList.jsx
 import React from 'react';
+import Event from "./Event";
 
 
-const EventList = () => {
-  return (
-    <div id="event-list"></div>
-  );
+const EventList = ({ events }) => {
+ return (
+   <ul id="event-list">
+     {events? events.map(event => <Event key={event.id} event={event} />): null}
+   </ul>
+ );
 }
 
 
 export default EventList;
+
