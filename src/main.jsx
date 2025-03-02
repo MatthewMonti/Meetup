8ca544c85274bfa55029af67463e8f0f603af34b
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2025-03-02 02:27:12 -0500

    Registering Your Service Worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App.jsx';
-import atatus from 'atatus-spa';
-atatus.config('73ba51b05269405eab4f5af2f046caf0').install();
+
+//import atatus from 'atatus-spa';
+//test functionality while deployed for speed and browser/OS combatiblity isssues
+//BELOW way to connect to atus
+//atatus.config('73ba51b05269405eab4f5af2f046caf0').install();
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js/index.js';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+serviceWorkerRegistration.register();
