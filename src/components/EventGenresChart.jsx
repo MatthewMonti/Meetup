d9472160049f9a22ad7eb0e1be2b101f9de1bc41
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2025-03-24 10:09:19 -0400

    I cannot solve tis problem stringify is garbage

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -8,7 +8,7 @@ const EventGenresChart = ({ allLocations, events }) => {
   const COLORS = ['#0088FE','#00C49F','#FFBB28','#FF8042','#8884d8'];
   useEffect(() => {
     setData(getData());
-  }, [`${events}`]);
+  }, [events]);
 
 
   const getData = () => {
