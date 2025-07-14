317411e05cbcdd22f144b937c8a30d862620d8e8
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2025-07-14 15:52:21 -0400

    purcomponent still too slow reset

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -1,57 +1,50 @@
-import React, { PureComponent } from 'react';
+// src/components/CityEventsChart.jsx
+
+import React, { useState, useEffect } from 'react';
 import {
   ScatterChart,
   Scatter,
   XAxis, YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
-  Label
+  ResponsiveContainer
 } from 'recharts';
 
-class CityEventsChart extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.getData(props.events, props.allLocations),
-    };
-  }
+const CityEventsChart = ({ allLocations, events }) => {
+  const [data, setData] = useState([]);
 
-  getData(events, allLocations) {
-    return allLocations.map((location) => {
-      const count = events.filter((event) => event.location === location).length;
-      const city = location.split(/, | - /)[0];
+  useEffect(() => {
+    setData(getData());
+  }, [`${data}`]);
+
+  const getData = () => {
+    const data = allLocations.map((location) => {
+      const count = events.filter((event) => event.location === location).length
+      const city = location.split(', ')[0]
       return { city, count };
-    });
-  }
+    })
+    return data;
+  };
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.events !== this.props.events) {
-      this.setState({ data: this.getData(this.props.events, this.props.allLocations) });
-    }
-  }
-
-  render() {
-    return (
-      <ResponsiveContainer width="50%" height={400}>
-        <ScatterChart
-          margin={{ top: 100, right: 50, bottom: 100, left: 40 }}
-        >
-          <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="City"
-            angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
-          >
-            <Label value="Cities" offset={-70} position="insideBottom" />
-          </XAxis>
-          <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false}>
-            <Label value="Number of Meetings" offset={-120} position="insideLeft" />
-          </YAxis>
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter name="A meeting" data={this.state.data} fill="#8884d8" />
-        </ScatterChart>
-      </ResponsiveContainer>
-    );
-  }
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <ScatterChart
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid />
+        <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+        <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter name="A school" data={data} fill="#8884d8" />
+      </ScatterChart>
+    </ResponsiveContainer>
+  );
 }
 
 export default CityEventsChart;
+
--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -1,46 +1,46 @@
-import React, { PureComponent } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
+import { useEffect, useState} from 'react';
 
-class EventGenresChart extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.getData(props.events),
-    };
-  }
+const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular' ];
+  const COLORS = ['#0088FE','#00C49F','#FFBB28','#FF8042','#8884d8'];
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter(event => event.summary.includes(genre));      
+      return {
+        name: genre,
+        value: filteredEvents.length
+      }
+    })
+    return data;
+  };
+  useEffect(() => {
+    setData(getData());
+  }, [events]);
 
-  genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
-  COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8'];
-
-  getData(events) {
-    return this.genres.map((genre) => ({
-      name: genre,
-      value: events.filter((event) => event.summary.includes(genre)).length,
-    }));
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.events !== this.props.events) {
-      this.setState({ data: this.getData(this.props.events) });
-    }
-  }
-
-  render() {
-    return (
-      <ResponsiveContainer width="50%" height={400}>
-        <PieChart width={400} height={400}>
-          <Pie data={this.state.data} labelLine={false} outerRadius={125} fill="#8884d8" dataKey="value">
-            {this.state.data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={this.COLORS[index % this.COLORS.length]} />
+  return (
+    <ResponsiveContainer width="50%" height={400}>
+      <PieChart width={400} height ={400} >
+        <Pie
+            data={data}
+            labelLine={false}
+            outerRadius={125}
+            fill="#8884d8"
+            dataKey="value"
+            label="name"
+        >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
-          </Pie>
-          <Legend verticalAlign="bottom" height={36} />
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  }
+        </Pie>
+        <Legend verticalAlign="bottom" height={36}/>
+      </PieChart>
+    </ResponsiveContainer>
+  );
 }
 
 export default EventGenresChart;
+
 
 
